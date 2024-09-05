8536eee19f192e23744f7a4045d707d1f0ef9ba0
roomey_rahman <roomeyrahman@gmail.com>
2024-09-05 14:44:34 +0600

    update db

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,8 +5,16 @@ export type TRole = ''
 
 export type TSubmission = {
   _id: ObjectId | string
-  name: string
   university: string
+  professorName: string
+  designation: string
+  researchInterests: string[]
+  website: string
+  email: string
+  mailingDate: Date
+  emailType: 'new' | 'reply' | 'followup'
+  priority: 'High' | 'Medium' | 'Low'
+  result: 'Positive' | 'Negative' | 'Neutral' | 'No Response'
   createdAt: Date
   updatedAt: Date
 }
