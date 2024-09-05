a5bf8e07a2934168a8481f122c05198e02fda4e0
sifatullahsu <personal.sifat@gmail.com>
2024-09-05 15:22:26 +0600

    submission schema

--- src/models/submission.model.ts
+++ src/models/submission.model.ts
@@ -1,18 +1,25 @@
-import { TSubmission } from "@/types";
-import mongoose, { Model } from "mongoose";
+import { TSubmission } from '@/types'
+import mongoose, { Model } from 'mongoose'
 
 const schema = new mongoose.Schema<TSubmission>(
   {
-    name: { type: String, trim: true, required: true },
     university: { type: String, trim: true, required: true },
+    professorName: { type: String, trim: true, required: true },
+    designation: { type: String, trim: true, required: true },
+    researchInterests: [{ type: String, trim: true, required: true }],
+    website: { type: String, trim: true },
+    email: { type: String, trim: true, required: true },
+    mailingDate: { type: Date, trim: true, required: true },
+    emailType: { type: String, enum: ['new', 'reply', 'followup'], default: 'new' },
+    priority: { type: String, enum: ['High', 'Medium', 'Low'], default: 'Low' },
+    result: { type: String, enum: ['Positive', 'Negative', 'Neutral', 'No Response'], default: 'No Response' }
   },
   {
     timestamps: true,
-    versionKey: false,
+    versionKey: false
   }
-);
+)
 
-const Submission: Model<TSubmission> =
-  mongoose.models.Submission || mongoose.model("Submission", schema);
+const Submission: Model<TSubmission> = mongoose.models.Submission || mongoose.model('Submission', schema)
 
-export default Submission;
+export default Submission
